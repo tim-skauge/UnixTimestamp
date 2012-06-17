16511323f714be13c52217cb9f0fe57586f4b5f4
Tim Skauge <tim@skauge.dk>
2012-06-17 10:27:47 +0200

    introduced UnixTimestamp project

---
+++ UnixTimestamp/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("UnixTimestamp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnixTimestamp")]
+[assembly: AssemblyCopyright("Copyright © mi7 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
