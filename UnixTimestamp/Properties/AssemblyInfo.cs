910233683e2b3a15a31955bf3c5ad01990fb54e1
Tim Skauge <tim@skauge.dk>
2012-07-06 10:07:17 +0200

    enabled CA1014: Mark assemblies with CLSCompliantAttribute

--- UnixTimestamp/Properties/AssemblyInfo.cs
+++ UnixTimestamp/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 [assembly: AssemblyTitle("UnixTimestamp")]
 [assembly: AssemblyDescription("UnixTimestamp is a small struct with one, an only one, purpose: To convert to/from UnixTimestamp in various situations.\r\n\r\nDocumentation and source can be on GitHub at https://github.com/mi7/UnixTimestamp")]
@@ -10,3 +11,4 @@
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("0.1.2.0")]
 [assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: CLSCompliant(true)]
