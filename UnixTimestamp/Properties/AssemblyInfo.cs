b6dff0c65385d814b3d218d5b7f6b697a5c22306
Tim Skauge <tim@skauge.dk>
2012-12-02 12:15:53 +0100

    updated nuspec and assembly info

--- UnixTimestamp/Properties/AssemblyInfo.cs
+++ UnixTimestamp/Properties/AssemblyInfo.cs
@@ -3,14 +3,14 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("UnixTimestamp")]
-[assembly: AssemblyDescription("UnixTimestamp is a small struct with one, an only one, purpose: To convert to/from UnixTimestamp in various situations.\r\n\r\nDocumentation and source can be on GitHub at https://github.com/mi7/UnixTimestamp")]
+[assembly: AssemblyDescription("UnixTimestamp is a small struct with one, an only one, purpose: To convert to/from UnixTimestamp in various situations.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tim Skauge")]
 [assembly: AssemblyProduct("UnixTimestamp")]
 [assembly: AssemblyCopyright("Copyright © Tim Skauge 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
